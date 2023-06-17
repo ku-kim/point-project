0e2ea165505f87a224b2be0332e647f949fe57e7
“kukim” <kukim.dev@gmail.com>
2023-06-17 16:14:46 +0900

    feat: point-api 공통 QueueMessageSender 서비스 구현

--- point-api/src/main/java/com/github/kukim/point/api/config/ApiAppConfig.java
+++ point-api/src/main/java/com/github/kukim/point/api/config/ApiAppConfig.java
@@ -1,11 +1,15 @@
 package com.github.kukim.point.api.config;
 
+import com.github.kukim.point.clients.aws.config.AwsResourceConfig;
 import com.github.kukim.point.core.config.CoreConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(CoreConfig.class)
+@Import({
+	CoreConfig.class,
+	AwsResourceConfig.class
+})
 public class ApiAppConfig {
 
 }
