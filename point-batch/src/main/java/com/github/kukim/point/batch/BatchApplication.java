31bf3e9065a065da836d207be50d1180805bbad0
“kukim” <kukim.dev@gmail.com>
2023-06-19 19:03:04 +0900

    chore: point-batch 설명

---
+++ point-batch/src/main/java/com/github/kukim/point/batch/BatchApplication.java
@@ -0,0 +1,11 @@
+package com.github.kukim.point.batch;
+
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		System.out.println("""
+			현재 Batch를 통한 포인트 유효기간 만료는 구현하지 않음
+			""");
+	}
+
+}
